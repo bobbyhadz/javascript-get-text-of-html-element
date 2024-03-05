bd7e88d6041d4b4f25167d83043c2c9c7d3c1ad2
bobbyhadz <opmftw@gmail.com>
2024-03-05 12:52:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const container = document.getElementById('container');
+
+// 👇️ One, Two, Three
+console.log(container.textContent);
+
+// 👇️ One, Two, Three
+console.log(container.innerText);
